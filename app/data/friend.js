ef86dbe52ba1c430956adbdec3e5931e77afa162
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-04-11 09:55:09 -0400

    Third Commit

---
+++ app/data/friend.js
@@ -0,0 +1,63 @@
+var friendsList = [
+	{
+        name: "Bruce Wayne",
+        photo: "https://i.pinimg.com/originals/04/96/68/049668884cd8f378b520e04e3f3fe3ee.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+
+	{
+        name: "Steve Rogers",
+        photo: "https://vignette.wikia.nocookie.net/marveldatabase/images/6/63/Steven_Rogers_%28Earth-22795%29.jpg/revision/latest?cb=20110914145159",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+
+	{
+        name: "Tony Stark",
+        photo: "http://25.media.tumblr.com/tumblr_m8trww0Qid1qlva78o1_500.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    
+    {
+        name: "Bruce Banner",
+        photo: "https://tse3.mm.bing.net/th?id=OIP.zZZQRdKWcr1lcGz9Vo-HRwHaKk&w=190&h=271&c=8&o=5&pid=1.7",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Thor",
+        photo: "https://media.wired.com/photos/59fb679f8e9fc3082a139c11/master/pass/Thor-MainArt.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Jean Grey",
+        photo: "https://www.dynamicforces.com/images/C126576.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Natasha Romanoff",
+        photo: "https://i.pinimg.com/originals/e9/1f/a7/e91fa769b8b415666c53acdc05a31a72.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Diana Prince",
+        photo: "https://i.pinimg.com/474x/72/4f/f9/724ff95315f15c87afb99c4dbb7ad4ca--comic-babes-dc-comic.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Carol Danvers",
+        photo: "https://static01.nyt.com/images/2019/03/10/arts/10marvel-list1/merlin_151018680_8b11e0d6-d5d0-4dc2-9b72-b5a46205b550-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+
+    {
+        name: "Ororo Munroe",
+        photo: "http://callieburrito.com/wp-content/uploads/2016/10/IMG_0566-50https://i.pinimg.com/736x/50/68/af/5068af3461b976d7e32b4a436fd9ea9f--storm-xmen-x-men.jpg0x310.jpg",
+        preferences: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+];
+
+module.exports = friendsList;
